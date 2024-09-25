856ee2cd504ebecda341e8864218aba156000f0d
Abhinav Tyagi <Abhinavtyagiup@gmail.com>
2024-09-25 08:34:58 +0530

    3

--- script/js/script.js
+++ script/js/script.js
@@ -1,22 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the current path of the HTML file
-    const currentPath = window.location.pathname;
-    
-    // Calculate how deep the current HTML file is in the folder structure
-    const depth = currentPath.split('/').length - 2; // "-2" to exclude the domain and file name
-    
-    // Build the correct relative path to navbar.html by adding "../" based on the depth
-    let navbarPath = "";
-    for (let i = 0; i < depth; i++) {
-        navbarPath += "../";
+class CustomNavbar extends HTMLElement {
+    connectedCallback() {
+        fetch('navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                this.innerHTML = data;
+            })
+            .catch(error => console.error('Error loading navbar:', error));
     }
-    navbarPath += "navbar.html";  // Add the navbar.html at the end
-
-    // Fetch the navbar.html and insert it into the page
-    fetch(navbarPath)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar").innerHTML = data;
-        })
-        .catch(error => console.error('Error fetching navbar:', error));
-});
+}
+customElements.define('custom-navbar', CustomNavbar);
